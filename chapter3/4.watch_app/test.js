9ca99256266088754db411725db3cce912e124b9
chechengpeng <chechengpeng@126.com>
2018-05-08 20:51:41 +0800

    chapter3

---
+++ chapter3/4.watch_app/test.js
@@ -0,0 +1,17 @@
+function asyncFunction(callback) {
+  setTimeout(callback, 200);
+}
+
+var color = 'blue';
+
+/* asyncFunction(function() {
+  console.log('The color is ' + color);
+}); */
+
+(function(color) {
+  asyncFunction(function() {
+    console.log('The color is ' + color);
+  });
+})(color);
+
+color = 'green';
